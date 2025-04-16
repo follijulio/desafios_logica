7c3cc498882844993365ac1f15dedd7b21cf0c30
follijulio <follijulio@gmail.com>
2025-04-16 09:35:35 -0300

    adiciona função calcularPontos para calcular pontuações com base em ações

---
+++ V2/JAVASCRIPT/contadorDePontos.js
@@ -0,0 +1,48 @@
+/**
+ * @param {number[]} acoes
+ */
+
+function calcularPontos(acoes) {
+  const pontuacoes = [
+    { pontuacao: 10, tipo: "coletou" },
+    { pontuacao: 20, tipo: "inimigo derrotado" },
+    { pontuacao: -5, tipo: "dano sofrido" },
+    { pontuacao: 5, tipo: "cura" },
+  ];
+
+  let soma = 0;
+
+  acoes.forEach((item) => {
+    const ponto = pontuacoes.find((pontuacao) => pontuacao.tipo === item);
+
+    soma += ponto ? (ponto.pontuacao ? ponto.pontuacao : 0) : 0;
+  });
+
+  return soma;
+}
+
+function main() {
+  const resultados = [];
+
+  resultados.push(
+    calcularPontos(["coletou", "inimigo derrotado", "dano sofrido"])
+  );
+  resultados.push(calcularPontos(["cura", "cura", "cura", "coletou"]));
+  resultados.push(calcularPontos(["inimigo derrotado", "inimigo derrotado"]));
+  resultados.push(
+    calcularPontos(["coletou", "voou", "teleportou", "dano sofrido"])
+  );
+  resultados.push(
+    calcularPontos([
+      "coletou",
+      "dano sofrido",
+      "cura",
+      "coletou",
+      "inimigo derrotado",
+    ])
+  );
+
+  //! ESPERADO 25, 25, 40, 5, 40
+  console.log(resultados);
+}
+main();
