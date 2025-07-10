ee95e57212af9f85691619553ddc1434110b353f
follijulio <follijulio@gmail.com>
2025-07-10 10:27:59 -0300

    Refactor longestCommonPrefix function to improve logic and update test cases

--- V2/LEETCODE/14_LongestCommonPrefix.js
+++ V2/LEETCODE/14_LongestCommonPrefix.js
@@ -4,15 +4,29 @@
  */
 
 var longestCommonPrefix = function (strs) {
-  console.log(strs[0[0]]);
-  let prefix = "";
 
-  console.log(prefix);
+  if (strs.length < 1 && strs.length <= 200) return "";
+
+  let prefix = strs[0];
+  let a = "";
+
+  strs.map((str) => {
+    a = "";
+    for (let i = 0; i < str.length; i++) {
+      if (str[i] === prefix[i]) {
+        a += str[i];
+      } else {
+        prefix = a;
+        break;
+      }
+    }
+  });
+  return prefix;
 };
 
 function main() {
-  longestCommonPrefix(["flower", "flow", "flight"]);
-  longestCommonPrefix(["dog", "racecar", "car"]);
+  a = longestCommonPrefix(["miau", "miau", "mira"]);
+  console.log(a);
 }
 
 main();
